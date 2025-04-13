9eaf84776090407d105991cac3882317b6eb7287
theg <52027622+theg1239@users.noreply.github.com>
2025-04-13 13:56:01 +0530

    import stuff

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth"
-import { authOptions } from "../options"
+import { authOptions } from '@/app/api/auth/options'
 
 // Extend the Session type to include custom properties
 declare module "next-auth" {
